33b5f51b476fbefe3846d8d076fed4f44f70fe0c
Dominick Baier <dbaier@leastprivilege.com>
2012-03-01 11:45:54 +0100

    clean

--- NuGet/pack.cmd
+++
@@ -1,2 +0,0 @@
-NuGet.exe pack .\Thinktecture.IdentityModel\Thinktecture.IdentityModel.nuspec -Symbols
-NuGet.exe pack .\Thinktecture.IdentityModel.Web\Thinktecture.IdentityModel.Web.nuspec -Symbols
